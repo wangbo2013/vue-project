ef2e16f02a3e54756064c84a22c75499a0ac3b6e
kingbo <493800661@qq.com>
2018-07-18 21:40:49 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-container>
+    <v-header></v-header>
+    <v-content></v-content>
+    <v-footer></v-footer>
+  </el-container>
+</template>
+<script>
+  import header from './components/header';
+  import content from './components/content';
+  import footer from './components/footer';
+export default {
+  name: 'app',
+  data(){
+      return {};
+  },
+  components: {
+      'v-header': header,
+      'v-content': content,
+      'v-footer': footer,
+  }
+};
+</script>
+<style lang="stylus" scoped>
+  .el-container{
+    width: 100%;
+    height: 100%;
+    border: 1px solid #808080;
+    display: flex;
+    display: -webkit-flex;
+    flex-direction: column;
+  }
+</style>
---
+++ src/components/footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-footer>
+    Copyright © 2013-2018 {{name}}  runoob.com All Rights Reserved. 备案号：闽ICP备15012807号-1
+  </el-footer>
+</template>
+<script>
+    export default{
+        data(){
+            return {
+                name: '菜鸟教程'
+            }
+        }
+    }
+</script>
+<style lang="stylus" scoped>
+  .el-footer{
+    background-color: #ebebeb;
+    color: #333;
+    text-align: center;
+    height: 40px !important;
+    line-height: 40px;
+  }
+</style>
